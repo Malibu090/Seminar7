af9a4cec985eeacdab527cbe4acc19e3b81f52ac
Malibu <malibu090@mail.ru>
2022-12-12 23:33:50 +0300

    Создать двумерный массив, заполненный случайными числами

---
+++ Task46/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(leftRange, rightRange);
+        }
+    }
+    return array;
+}
+
+int EnterNumber(string message)
+{
+    System.Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+// получить переменные m и n от пользователя
+
+int m = EnterNumber("Input number m: ");
+
+int n = EnterNumber("Input number n: ");
+
+// Create double digit array (Создать двумерный массив)
+
+int[,] matrix = CreateRandomArray(m, n, 1, 10);
+
+// Output array
+
+ShowArray(matrix);
