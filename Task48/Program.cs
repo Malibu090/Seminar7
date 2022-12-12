e59604b306ccbbb55ae610f074dcfad2817a5370
Malibu <malibu090@mail.ru>
2022-12-12 23:35:15 +0300

    Массив, каждый элемент которого выводтся по формуле

---
+++ Task48/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = i + j;
+        }
+    }
+    return array;
+}
+
+int EnterNumber(string message)
+{
+    System.Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+// получить переменные m и n от пользователя
+
+int m = EnterNumber("Input number m: ");
+
+int n = EnterNumber("Input number n: ");
+
+// Create double digit array (Создать двумерный массив)
+
+int[,] matrix = CreateRandomArray(m, n, 1, 10);
+
+// Output array
+
+ShowArray(matrix);
