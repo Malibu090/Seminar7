0a8a0ebebd6b95aefd4801a2dd7294ef68b4eee2
Malibu <malibu090@mail.ru>
2022-12-12 23:37:08 +0300

    Новый массив элементы с двумя четными индексами возведены в квадрат

---
+++ Task49/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+int[,] ConverseEven(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 & j % 2 ==0)
+            {
+                matrix[i, j] *= matrix[i, j];
+            }
+        }
+    }
+    return matrix;
+}
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns]; 
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(leftRange, rightRange);
+        }
+    }
+    return array;
+}
+
+int EnterNumber(string message)
+{
+    System.Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+// получить переменные m и n от пользователя
+
+int m = EnterNumber("Input number m: ");
+
+int n = EnterNumber("Input number n: ");
+
+// Create double digit array (Создать двумерный массив)
+
+int[,] matrix = CreateRandomArray(m, n, 1, 10);
+
+// Output array
+
+ShowArray(matrix);
+
+ConverseEven(matrix);
+System.Console.WriteLine();
+ShowArray(matrix);
