7f681b61d94410b9d4a040413c2edb8a1024fcc9
Malibu <malibu090@mail.ru>
2022-12-12 23:38:18 +0300

    Случайный массив в котором надо найти сумму главной диагонали

---
+++ Task51/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 51
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int SumDiagonal(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum += matrix[i, i];
+    }
+    return sum;
+}
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(leftRange, rightRange);
+        }
+    }
+    return array;
+}
+
+int EnterNumber(string message)
+{
+    System.Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+// получить переменные m и n от пользователя
+
+int m = EnterNumber("Input number m: ");
+
+int n = EnterNumber("Input number n: ");
+
+// Create double digit array (Создать двумерный массив)
+
+int[,] matrix = CreateRandomArray(m, n, 1, 10);
+
+// Output array
+
+ShowArray(matrix);
+
+int result = SumDiagonal(matrix);
+
+System.Console.WriteLine("Сумма элементов главной диагонали: " + result);
